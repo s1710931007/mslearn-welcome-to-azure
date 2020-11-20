524a83c8590beff1ec6a1eb5dd1b105438c13929
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-11-20 19:44:46 +0800

    fix

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Welcome to Azure! My name is QQnice 2 1104 $($env:computername).</h2></body></html>"
+  -Value '<html><body><h2>Welcome to Azure! My name is AChun 20201120 $($env:computername).</h2> <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiOTYzZWRiZjYtMWJlYS00OTA2LThhZWUtMTkxMmQ0YmFmYzAxIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></body></html>'
